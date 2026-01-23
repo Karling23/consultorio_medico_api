61b30cc981409eeb7a6bb482c8ca1195842e9464
Karling23 <clalquinga@gmail.com>
2026-01-23 13:05:12 -0500

    .

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,30 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { GlobalHttpExceptionFilter } from './common/filters/http-exception.filter';
 import { join } from 'path';
-import { NestExpressApplication } from '@nestjs/platform-express'; // 👈 Importación necesaria
+import { NestExpressApplication } from '@nestjs/platform-express'; 
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule); // 👈 Tipo específico
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+  app.enableCors({
+    origin: [
+    'http://localhost:5173',
+    'https://alquinga-consultorio-medico-api.desarrollo-software.xyz',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: false,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   app.useGlobalFilters(new GlobalHttpExceptionFilter());
 
-  app.useStaticAssets(join(__dirname, '..', 'public')); // 👈 Habilita acceso público a /public
+  app.useStaticAssets(join(__dirname, '..', 'public'));
 
   await app.listen(3000);
 }
