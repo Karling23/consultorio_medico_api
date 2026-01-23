614a6f9b7114480b6ebf742444dfb9e35dab3b53
Karling23 <clalquinga@gmail.com>
2026-01-23 13:12:07 -0500

    main correccion

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,6 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.enableCors({
-    origin: [
-    'http://localhost:5173',
-    'https://alquinga-consultorio-medico-api.desarrollo-software.xyz',
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: false,
-  });
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
